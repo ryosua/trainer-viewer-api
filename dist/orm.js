cd23254f396884f192bf18410a3fcdecbcf8681b
ryosua <hi@ryanyosua.me>
2020-04-10 08:44:49 -0600

    remove files

--- dist/orm.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var sequelize_1 = require("sequelize");
-var sequelize = new sequelize_1.Sequelize(process.env.DATABASE_URI || '');
-exports.default = sequelize;
