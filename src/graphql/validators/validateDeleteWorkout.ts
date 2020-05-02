5b041f6d601d82c21c6b59dfa255209a5d2c9db7
ryosua <hi@ryanyosua.me>
2020-05-02 12:10:52 -0600

    fix validate delete workout

--- src/graphql/validators/validateDeleteWorkout.ts
+++ src/graphql/validators/validateDeleteWorkout.ts
@@ -2,7 +2,7 @@ import User from '../types/User'
 import Workout from '../types/Workout'
 
 const validateDeleteWorkout = (user: User, workout: Workout): void => {
-    const isAuthorized = user.id === workout.id
+    const isAuthorized = user.id === workout.trainer.id
     if (!isAuthorized) {
         throw new Error('You can only delete workouts that you have created.')
     }
