1c37baa04540d6d4db3c2254d1b6782bad2bb36f
ryosua <hi@ryanyosua.me>
2020-05-09 14:50:35 -0600

    fix sequalize hanging

--- src/index.ts
+++ src/index.ts
@@ -17,18 +17,18 @@ graphqlServer.listen({ port: process.env.PORT || 4000 }).then(({ url }: any) => 
         migrationDirectory: './migrations'
     })
 
-    postgrator
-        .migrate()
-        .then((appliedMigrations: any) => {
-            console.log('Applied migrations: ', appliedMigrations)
-            orm.authenticate()
-                .then(() => {
-                    console.log('Connection has been established successfully.')
-                    wakeUpDyno(process.env.DYNO_URL ?? '')
+    orm.authenticate()
+        .then(() => {
+            postgrator
+                .migrate()
+                .then((appliedMigrations: any) => {
+                    console.log('Applied migrations: ', appliedMigrations)
                 })
-                .catch((err: Error) => {
-                    console.error('Unable to connect to the database:', err)
-                })
+                .catch((error: Error) => console.log(error))
+
+            wakeUpDyno(process.env.DYNO_URL ?? '')
         })
-        .catch((error: Error) => console.log(error))
+        .catch((err: Error) => {
+            console.error('Unable to connect to the database:', err)
+        })
 })
