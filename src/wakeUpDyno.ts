ef771af6aa8c4f27245722916121baa8ea994f8a
ryosua <hi@ryanyosua.me>
2020-05-20 17:06:28 -0600

    only wake dyno if env var is set

--- src/wakeUpDyno.ts
+++ src/wakeUpDyno.ts
@@ -1,26 +1,28 @@
 import fetch from 'node-fetch'
 
 const wakeUpDyno = (url: string, interval = 25, callback = () => {}) => {
-    const milliseconds = interval * 60000
-    setTimeout(() => {
-        try {
-            console.log(`waking up dyno`)
-            // HTTP GET request to the dyno's url
-            fetch(url).then(() => console.log(`Fetching ${url}.`))
-        } catch (e) {
-            // catch fetch errors
-            console.log(`Error fetching ${url}: ${e.message} 
-            Will try again in ${interval} minutes...`)
-        } finally {
+    if (process.env.WAKE_DYNO) {
+        const milliseconds = interval * 60000
+        setTimeout(() => {
             try {
-                callback()
+                console.log(`waking up dyno`)
+                // HTTP GET request to the dyno's url
+                fetch(url).then(() => console.log(`Fetching ${url}.`))
             } catch (e) {
-                callback ? console.log('Callback failed: ', e.message) : null
+                // catch fetch errors
+                console.log(`Error fetching ${url}: ${e.message} 
+                Will try again in ${interval} minutes...`)
             } finally {
-                return wakeUpDyno(url, interval, callback)
+                try {
+                    callback()
+                } catch (e) {
+                    callback ? console.log('Callback failed: ', e.message) : null
+                } finally {
+                    return wakeUpDyno(url, interval, callback)
+                }
             }
-        }
-    }, milliseconds)
+        }, milliseconds)
+    }
 }
 
 export default wakeUpDyno
